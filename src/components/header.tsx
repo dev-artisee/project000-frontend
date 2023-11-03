8b500dceab305d31796e96eeb14ab743a59348de
Dongwon Kang <dw3624@gmail.com>
2023-11-03 22:31:44 +0900

    feat: add header

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header className="w-full h-14">
+      <div className="container h-full flex justify-between items-center">
+        <div className="font-bold">픽밀업</div>
+        <div className="text-sm">로그인</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
